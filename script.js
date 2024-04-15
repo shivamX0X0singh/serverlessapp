222b1da3923ce0040bfa4a1df4a6891414cdc143
shivamX0X0singh <165036390+shivamX0X0singh@users.noreply.github.com>
2024-04-15 19:33:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' 
     },
     api: {
-        invokeUrl: '' 
+        invokeUrl: 'https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_161n8c7pr/.well-known/jwks.json',
     }
 };
