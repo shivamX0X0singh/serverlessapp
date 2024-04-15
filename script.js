82270b126369dd9bbd6642770d5a25bc2d675a7a
shivamX0X0singh <165036390+shivamX0X0singh@users.noreply.github.com>
2024-04-15 11:26:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-const form = document.querySelector('form');
-const greeting = document.querySelector('#greeting');
-
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const name = document.querySelector('#name').value;
-	greeting.textContent = `Hello, ${name}!`;
-});
-
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch(
-        "https://o73g5ptpujgtclinhzhhneplje0jogoh.lambda-url.us-east-1.on.aws/"
-    );
-    let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;
-}
-updateCounter();
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_161n8c7pr', 
+        userPoolClientId: '2423lslgdutmbo5t837jtgdj7f', 
+        region: 'ap-south-1' 
+    },
+    api: {
+        invokeUrl: '' 
+    }
+};
